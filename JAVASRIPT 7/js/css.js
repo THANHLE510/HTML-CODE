67bf8968d53e6a2ba17fe180bc72f5bf7cec2c18
THANHLE510 <111995033+THANHLE510@users.noreply.github.com>
2022-10-20 21:30:39 +0700

    Add files via upload

---
+++ JAVASRIPT 7/js/css.js
@@ -0,0 +1,15 @@
+$(function (){
+
+    var backgroundColor = $('li').css('background-color');
+
+    $('ul').append('<p>Color was: ' + backgroundColor + '</p>');
+
+    $('li').css({
+        'background-color' : '#c5a996',
+        'border' : '1px solid #fff',
+        'color' : '#000',
+        'font-family' : 'Georgia',
+        'text-shadow' : 'none',
+        'padding-left' : '++75'
+    });
+});
